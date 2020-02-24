2b2ca835d5e0b318b6815f05f98b423d09ecc6d6
eiTanLaVi <eitan@2key.network>
2020-02-24 13:46:43 +0200

    update AC api with requirements

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='activecampaign-python',
-      version='1.0.0',
+setup(name='acpy',
+      version='1.0.1',
       description='ActiveCampaign API written in python',
       long_description=read('README.md'),
-      author='Miguel Ferrer',
-      author_email='ingferrermiguel@gmail.com',
-      url='https://github.com/GearPlug/activecampaign-python',
+      author='Miguel Ferrer, 2key.network',
+      author_email='hello@2key.network',
+      url='https://github.com/2key/activecampaign-python',
       packages=['activecampaign'],
       install_requires=[
           'requests',
